c1f8e2da6776a09266ec81eb299e3a7373508a06
armin11 <armin.retterath@gmail.com>
2020-10-07 09:28:53 +0200

    Quickfix - delete linebreak

--- http/php/mod_inspireAtomFeedISOMetadata.php
+++ http/php/mod_inspireAtomFeedISOMetadata.php
@@ -1192,8 +1192,7 @@ SQL;
 	// srv DCP **************************************
 	$DCP = $iso19139->createElement ( "srv:DCP" );
 	$DCPList = $iso19139->createElement ( "srv:DCPList" );
-	$DCPList->setAttribute ( "codeList", "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schema
-s/resources/codelist/ML_gmxCodelists.xml#DCPList" );
+	$DCPList->setAttribute ( "codeList", "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/ML_gmxCodelists.xml#DCPList" );
 	$DCPList->setAttribute ( "codeListValue", "WebService" );
 	
 	$DCP->appendChild ( $DCPList );
