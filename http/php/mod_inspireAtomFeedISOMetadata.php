43c2801e925d14f050937d8ae41ad950e9768c14
armin11 <armin.retterath@gmail.com>
2020-10-07 09:18:38 +0200

    Quickfix for service metadata to fullfil demands of gdi-de testsuite and some inspire demands

--- http/php/mod_inspireAtomFeedISOMetadata.php
+++ http/php/mod_inspireAtomFeedISOMetadata.php
@@ -1192,7 +1192,8 @@ SQL;
 	// srv DCP **************************************
 	$DCP = $iso19139->createElement ( "srv:DCP" );
 	$DCPList = $iso19139->createElement ( "srv:DCPList" );
-	$DCPList->setAttribute ( "codeList", "DCPList" );
+	$DCPList->setAttribute ( "codeList", "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schema
+s/resources/codelist/ML_gmxCodelists.xml#DCPList" );
 	$DCPList->setAttribute ( "codeListValue", "WebService" );
 	
 	$DCP->appendChild ( $DCPList );
