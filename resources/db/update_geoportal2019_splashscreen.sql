c77681b8aba033196a9e01636283ecf224ad6844
karlo <karl.owesny@csoffm.de>
2021-03-25 15:02:59 +0100

    provide splashscreen sql update for geoportal-Hessen-2019

---
+++ resources/db/update_geoportal2019_splashscreen.sql
@@ -0,0 +1,1 @@
+UPDATE gui_element_vars SET var_value = '../include/geoportal_logo_splash.php' WHERE fkey_gui_id = 'Geoportal-Hessen-2019' AND fkey_e_id = 'body' AND var_name = 'includeWhileLoading';
