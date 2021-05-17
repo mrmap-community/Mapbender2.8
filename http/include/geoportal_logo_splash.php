38dc61da5f54aa8d8888bf19d974b37fbf5df5f6
karlo <karl.owesny@csoffm.de>
2021-05-17 02:06:19 -0700

    „http/include/geoportal_logo_splash.php“ ändern

--- http/include/geoportal_logo_splash.php
+++ http/include/geoportal_logo_splash.php
@@ -5,7 +5,7 @@ echo	"<table style='padding-top:100px;margin-top:-15px' width='100%'><tr align='
 	"<tr align='center'><td><p style='color:black;font-size:12.8px;font-family:Verdana,Sans Serif !important'>Lade Anwendung: " . $this->guiId . "</p></td></tr>".
 	"<tr align='center'><td style='padding:0px 0 0 0'><p style='color:black;font-size:12.8px;font-family:Verdana,Sans Serif !important;'>" .
 	"Bei Problemen folgen Sie diesem " .
-	"<a style='font-size:12.8px;text-decoration: underline;color:red;font-family:Verdana,Sans Serif !important;' href='http://".$_SERVER['HTTP_HOST']."/portal/hilfe/browsereinstellungen.html' target='_blank'>Link</a></p></td></tr>" .
+	"<a style='font-size:12.8px;text-decoration: underline;color:red;font-family:Verdana,Sans Serif !important;' href='https://".$_SERVER['HTTP_HOST']."/article/Browsereinstellung' target='_blank'>Link</a></p></td></tr>" .
 //	"<tr align='center'><td style='padding:50px 0 0 0'><img alt='logo' src='../img/Mapbender2_poweredby_logo_129x30.png'></td></tr>
 	"</table>";
 ?>
