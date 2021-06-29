d69d1d7b4b00785e3a597de54b0a18db130ab329
karlbrink <karl.owesny@csoffm.de>
2021-06-29 08:59:32 +0200

    rearranged the form structure and redesign with bootstrap.css

--- http/php/mod_loadCapabilities.php
+++ http/php/mod_loadCapabilities.php
@@ -26,23 +26,25 @@ require_once dirname(__FILE__) . "/../php/mb_validatePermission.php";
 $guiList = $_POST["guiList"];
 $xml_file = $_POST["xml_file"];
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
 <html>
 <head>
 <?php
 echo '<meta http-equiv="Content-Type" content="text/html; charset='.CHARSET.'">';	
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
 ?>
 <title>Load WMS</title>
 <?php
 include '../include/dyn_css.php';
 ?>
+<link rel="stylesheet" href="../extensions/bootstrap-3.3.6-dist/css/bootstrap.min.css" type="text/css">
 <style type="text/css">
   	<!--
   	body{
       background-color: #ffffff;
   		font-family: Arial, Helvetica, sans-serif;
-  		font-size : 12px;
-  		color: #808080
+  		font-size : 14px;
+  		color: #303030
   	}
   	.list_guis{
   		font-family: Arial, Helvetica, sans-serif;
@@ -68,6 +70,9 @@ include '../include/dyn_css.php';
   		font-size : 12px;
   	}
   	-->
+	#optionsbox,#newCapabilitiesBox {border: 1px solid #ccc;padding: 15px;border-radius: 4px;background-color: #efefef;margin-top: 30px;margin-bottom: 30px;}
+	#authbox {border: 1px solid #ccc;max-width: 300px;padding: 15px;border-radius: 4px;background-color: #efefef;}
+	#authbox > .radio {margin: unset;}
 </style>
 <script language="JavaScript">
 function validate(wert){
@@ -84,10 +89,24 @@ function validate(wert){
 		}
 	}
 }
+function toggleAuthDivVis() { 
+	//alert(getRadioValue(document.form1.auth_type)); 
+ 	if (getRadioValue(document.form1.auth_type) != 'none') { 
+ 		document.getElementById("imrAuthDiv").style.display = "block"; 
+ 	} else { 
+ 		document.getElementById("imrAuthDiv").style.display = "none"; 
+ 	} 
+ } 
+ 		 
+ function getRadioValue(rObj) { 
+ 	for (var i=0; i<rObj.length; i++) if (rObj[i].checked) return rObj[i].value; 
+ 		return false; 
+ 	}
+
 </script>
 </head>
 <body>
-
+<div class="container" style="padding-top:15px;padding-bottom:15px;">
 <?php
 
 require_once(dirname(__FILE__)."/../classes/class_administration.php");
@@ -95,12 +114,7 @@ $admin = new administration();
 $ownguis = $admin->getGuisByOwner(Mapbender::session()->get("mb_user_id"),true);
 
 echo "<form name='form1' action='" . $self ."' method='post'>";
-echo "<table cellpadding='0' cellspacing='0' border='0'>";
-echo "<tr>";
-echo "<td>";
 if (count($ownguis)>0){
-	echo"GUI";
-	echo"<br>";
 	$v = array();
 	$t = array();
 	$sql = "SELECT * FROM gui WHERE gui_id IN ("; 
@@ -112,7 +126,8 @@ if (count($ownguis)>0){
 	}
 	$sql .= ") ORDER BY gui_name";
 	$res = db_prep_query($sql,$v,$t);
-	echo"<select size='8' name='guiList' style='width:200px' onClick='submit()'>";
+	echo"<div id='optionsbox' style='margin-top:0'><label for='guiList'>Wählen Sie Ihren Container aus</label><select class='form-control' name='guiList' onchange='submit()'>";
+	echo "<option value=''></option>";
 	while($row = db_fetch_array($res)){
 		echo "<option value='".$row["gui_id"]."' ";
 		if($guiList && $guiList == $row["gui_name"]){
@@ -122,11 +137,7 @@ if (count($ownguis)>0){
 	} 
 	$arrayGUIs = Mapbender::session()->get("mb_user_id");
 	echo count($arrayGUIs);
-	echo "</select><br><br>";
-	echo "</td>";
-	echo "<td>";
-	echo"WMS";
-	echo"<br>";
+	echo "</select>";
 	
 	if(isset($guiList) && $guiList!=""){
 		$sql = "SELECT DISTINCT wms.wms_title,gui_wms_position from gui_wms JOIN ";
@@ -136,7 +147,7 @@ if (count($ownguis)>0){
 		$t = array('s');
 		$res = db_prep_query($sql,$v,$t);
 		$count=0;
-		echo"<select size='8' name='wmsList' style='width:500px'>";
+		echo"<label style='margin-top: 15px;' for='wmsList'>Bereits enthaltene WMS</label><select class='form-control' name='wmsList' size='5' readonly>";
 	
 		while($row = db_fetch_array($res)){
 			if ($row["wms_title"]!=""){
@@ -145,51 +156,53 @@ if (count($ownguis)>0){
 			}
 			$count++;
 		}
-	    echo "</select><br><br>";
+	    echo "</select></div>";
 	}
 	else{
-		echo"<select size='8' name='wmsList' style='width:500px' on Click='submit()'>";
-		echo "</select><br><br>";
+		echo"<label style='margin-top: 15px;' for='wmsList'>Bereits enthaltene WMS</label><select class='form-control' name='wmsList' on Click='submit()'>";
+		echo "</select></div>";
 	}
-	echo "</td>";
-	echo "<tr></table><br>";
-	echo "Add the following REQUEST to the Online Resource URL to obtain the Capabilities document:<br>";
-	echo "<i>(Triple click to select and copy)</i><br>"; 
-	echo "REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.1.1<br>";
-	echo "REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.1.0<br>";
-	echo "REQUEST=capabilities&WMTVER=1.0.0<br><br>";
-	echo "Link to WMS Capabilities URL (Note: Registrating of http-secured services only possible if the <b>curl library</b>  is used for connections - defined in mapbender.conf):<br>";
 	#echo "Load WMS capabilities URL:<br>"
-	
+
+	echo "<div id='newCapabilitiesBox' class='' ><label for='xml_file'>Neue URL:</label>"; 
+
 	if (isset($xml_file)){
-		echo"<input type='text' name='xml_file' size='100' value='".$xml_file."'><br>";
+		echo"<input class='form-control' type='text' name='xml_file' value='".$xml_file."' placeholder='https://'>";
+		echo "<span id='helpBlock' class='help-block bg-danger' style='padding:10px;margin-top:25px;word-wrap:break-word;border-radius:4px;'>Die URL muss ein valides WMS Capabilities Dokument der Version 1.1.1 liefern. In der Regel sollte folgendes in Ihrer URL enthalten sein:<p style='margin:10px 0 0 0;font-weight:bold;'>REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.1.1</p></span></div>";
 	}else{
-		echo"<input type='text' name='xml_file' size='100' value='http://'><br>";
+		echo"<input class='form-control' type='text' name='xml_file' value='' placeholder='https://'>";
+		echo "<span id='helpBlock' class='help-block bg-danger' style='padding:10px;margin-top:25px;word-wrap:break-word;border-radius:4px;'>Die URL muss ein valides WMS Capabilities Dokument der Version 1.1.1 liefern. In der Regel sollte folgendes in Ihrer URL enthalten sein:<p style='margin:10px 0 0 0;font-weight:bold;'>REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.1.1</p></span></div>";
 	}
 	//show fields for authentication - only possible if curl is used as connector!
 	if (CONNECTION == 'curl') {
-		echo"HTTP Authentication:<br>";
-		echo"<input type='radio' name='auth_type' checked='checked' value='none'>None<br>";
-		echo"<input type='radio' name='auth_type' value='digest'>Digest<br>";
-    		echo"<input type='radio' name='auth_type' value='basic'>Basic<br>";
-		echo"Username<br>";
-		echo"<input type='text' name='username' size='50' value=''><br>";
-		echo"Password:<br>";
-		echo"<input type='text' name='password' size='50' value=''><br>";
+		echo"<div id='authbox' class=''><p><b>HTTP Authentication</b></p>";
+		echo"<div class='radio'><label><input type='radio' name='auth_type' checked='checked' value='none' onclick='toggleAuthDivVis();' >None</label></div>";
+		echo"<div class='radio'><label><input type='radio' name='auth_type' value='digest' onclick='toggleAuthDivVis();' >Digest</label></div>";
+    		echo"<div class='radio'><label><input type='radio' name='auth_type' value='basic' onclick='toggleAuthDivVis();' >Basic</label></div>";
+
+		echo "<div id='imrAuthDiv' style='display: none;'>";
+        echo "<label for='username'>Benutzername</label><input class='form-control' type='text' name='username' id='username' value=''>";
+        echo "<label for='password'>Passwort</label><input class='form-control' type='text' name='password' id='password' value=''>";
+        echo "</div>";
+
 	}
-	echo "<input type='checkbox' name='harvest_dataset_metadata' checked='checked'>"._mb('Harvest coupled dataset metadata from given MetadataURL tags')."<br>";
+
+	echo "</div><div id='optionsbox' class=''><p><b>Optionen</b></p>";
+	echo "<div class='checkbox'><label><input type='checkbox' name='harvest_dataset_metadata' id='harvest_dataset_metadata' checked='checked'>Originär verknüpfte Metadaten harvesten</label></div>";
+
 	if (defined("TWITTER_NEWS") && TWITTER_NEWS == true) {
-		echo"<input type='checkbox' name='twitter_news' checked='checked'>Publish via Twitter<br>";
+		echo"<div class='checkbox' style='display:none'><label><input type='checkbox' name='twitter_news' checked='checked'>Publish via Twitter</label></div>";
 	}
 	if (defined("GEO_RSS_FILE") &&  GEO_RSS_FILE != "") {
-		echo"<input type='checkbox' name='rss_news' checked='checked'>Publish via RSS<br>";
+		echo"<div class='checkbox' style='display:none'><label><input type='checkbox' name='rss_news' checked='checked'>Publish via RSS</label></div>";
 	}
-	echo"<input type='button' name='loadCap' value='Load' onClick='validate(\"guiList\")'>";	
+	echo"</div><input class='btn btn-primary' type='button' name='loadCap' value='Load' onClick='validate(\"guiList\")'>";	
 	echo "</form>";
 }
 else{
 	echo "There are no guis available for this user. Please create a gui first.";
 }
 ?>
+</div>
 </body>
 </html>
