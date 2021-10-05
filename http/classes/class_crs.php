0a4233ad5274dfb915bfd4e697ffdbd2208392fe
Armin Retterath <armin.retterath@gmail.com>
2021-10-05 15:21:45 +0200

    Update class_crs.php
    Set ttl for crs cache to one day.

--- http/classes/class_crs.php
+++ http/classes/class_crs.php
@@ -283,7 +283,7 @@ class Crs {
 		}
 		//store information - maybe to cache, if it does not already exists!
 		if ($cache->isActive && $cache->cachedVariableExists(md5($this->identifier)) == false) {
-			$cache->cachedVariableAdd(md5($this->identifier), json_encode($jsonCrsInfo));
+			$cache->cachedVariableAdd(md5($this->identifier), json_encode($jsonCrsInfo), 86400);
 			$e = new mb_notice("http/classes/class_crs.php - store crs info to cache!");
 			return true;
 		}
