b6a28b915e0fad469406a8f701da82065a4b21ec
Armin Retterath <armin.retterath@googlemail.com>
2021-11-12 13:51:36 +0100

    Add files via upload
    Fix - use local leaflet - was altered before ;-)

--- http/php/mod_linkedDataProxy.php
+++ http/php/mod_linkedDataProxy.php
@@ -2813,9 +2813,14 @@ switch ($f) {
 		$html .= '<meta name="viewport" content="width=device-width, initial-scale=1.0">' . $newline;
 		// $html .= '<link rel="shortcut icon" type="image/x-icon" href="" />';
 		// leaflet css
-		$html .= '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>' . $newline;
+		//$html .= '<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>' . $newline;
 		// leaflet js
-		$html .= '<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>' . $newline;
+		//$html .= '<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>' . $newline;
+		// local leaflet
+		$html .= '<link rel="stylesheet" href="/mapbender/extensions/leaflet-1.5.1/leaflet.css"/>' . $newline;
+		$html .= '<script src="/mapbender/extensions/leaflet-1.5.1/leaflet.js"></script>' . $newline;
+		
+		
 		// bootstrap
 		if ($useInternalBootstrap == true) {
 			if ($behindRewrite == true) {
