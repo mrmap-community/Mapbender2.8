c660ce99d12649c1d7be67418402b366e6271827
Armin Retterath <armin.retterath@gmail.com>
2022-09-20 12:21:16 +0200

    Update class_stripRequest.php
    Fix problem with format = image/png8 when trying to export geotiff from mapbender client

--- http/classes/class_stripRequest.php
+++ http/classes/class_stripRequest.php
@@ -94,7 +94,7 @@ class stripRequest{
 	function getFormat(){
 		$format = $this->get("format");
 			
-		if ($format=="PNG" || $format=="image/png"){
+		if ($format=="PNG" || $format=="image/png" || $format=="image/png8"){
 			return "png";
 		} else if ($format=="JPEG" || $format=="image/jpeg"){
 			return "jpeg";
