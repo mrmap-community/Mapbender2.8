6d1b8a1924b1f6e48704f7a1a7f89ac40ef86f50
Armin Retterath <armin.retterath@googlemail.com>
2024-04-08 17:21:02 +0200

    New select options source
    selectWfs - e.g.:  
    {
      "wfs_id": 1,
      "featuretype_id": 2,
      "element_ids": [1, 3],
      "select_id": "test123",
      "option_value_element_ids_indexes": [0],
      "option_text_element_ids_indexes": [0, 1],
      "option_empty": false,
      "option_text_mask": "{element_0} ({element_1})" 
    }

--- http/javascripts/mod_wfsGazetteerEditor_client.php
+++ http/javascripts/mod_wfsGazetteerEditor_client.php
@@ -545,6 +545,20 @@ function isSearchPreconfigured () {
 	return false;
 }
 
+
+function testJSON(text){
+	    if (typeof text!=="string"){
+	        return false;
+	    }
+	    try{
+	        var json = JSON.parse(text);
+	        return (typeof json === 'object');
+	    }
+	    catch (error){
+	        return false;
+	    }
+}
+
 function appendWfsForm() {
 	var form = document.getElementById("wfsForm");
 	removeChildNodes(form);
@@ -577,7 +591,29 @@ function appendWfsForm() {
 				spanNode.innerHTML = wfsConfElementArray[i].f_label;
 				if (wfsConfElementArray[i].f_form_element_html && wfsConfElementArray[i].f_form_element_html.length > 0) {
 					var inputNode = document.createElement("span");
-					inputNode.innerHTML = wfsConfElementArray[i].f_form_element_html;
+					//test if information is json encoded
+					if (testJSON(wfsConfElementArray[i].f_form_element_html)) {
+						//invoke select html						
+						var req = new parent.Mapbender.Ajax.Request({
+                    		url: 	"../php/mod_wfsElementSelect.php",
+                    		method:	"getSelectField",
+                    		async: false,
+                    		parameters: {
+                    			data: wfsConfElementArray[i].f_form_element_html
+                    		},
+                    		callback: (function(result, success, message){
+                    			if (success) {
+                    				inputNode.innerHTML = result.select;
+                    			} else {
+                        			console.log(message);
+                        			alert("a problem occured!");
+                    			}
+                    		})
+                    	});
+						req.send();
+					} else {
+						inputNode.innerHTML = wfsConfElementArray[i].f_form_element_html;
+					}
 				} else {
 					var inputNode = document.createElement("input");
 					inputNode.type = "text";
