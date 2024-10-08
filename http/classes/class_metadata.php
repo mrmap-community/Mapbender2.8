0dd5b07e5fbb13ccea7812cb536da37a2acb1f13
armin11 <armin.retterath@gmail.com>
2024-10-08 16:39:38 +0200

    Fix missing distributions in catalogue, when dataset has no coupled layers and/or featuretypes

--- http/classes/class_metadata.php
+++ http/classes/class_metadata.php
@@ -777,7 +777,16 @@ class searchMetadata
 					$downloadOptionsFromMetadata = json_decode(getDownloadOptions(array($datasetMatrix[$i]['fileidentifier']), $this->protocol . "://" . $this->hostName . "/mapbender/"));
 					//try to load coupled atom feeds from mod_getDownloadOptions and add them to result list! (if no wms layer nor wfs featuretype is available)
 					foreach ($downloadOptionsFromMetadata->{$datasetMatrix[$i]['fileidentifier']}->option as $dlOption) {
-						if ($dlOption->type == "downloadlink") {
+						if ($dlOption->type == "downloadlink" || $dlOption->type == "distribution") {
+							$this->datasetJSON->dataset->srv[$i]->coupledResources->inspireAtomFeeds[] = $dlOption;
+						}
+					}
+				}
+				if ($featuretypeCount == 0 && $layerCount == 0) {
+					$downloadOptionsFromMetadata = json_decode(getDownloadOptions(array($datasetMatrix[$i]['fileidentifier']), $this->protocol . "://" . $this->hostName . "/mapbender/"));
+					//try to load coupled atom feeds from mod_getDownloadOptions and add them to result list! (if no wms layer nor wfs featuretype is available)
+					foreach ($downloadOptionsFromMetadata->{$datasetMatrix[$i]['fileidentifier']}->option as $dlOption) {
+						if ($dlOption->type == "downloadlink" || $dlOption->type == "distribution") {
 							$this->datasetJSON->dataset->srv[$i]->coupledResources->inspireAtomFeeds[] = $dlOption;
 						}
 					}
